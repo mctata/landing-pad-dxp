ec06ccca60d52c37a210722885a4cf68bc54f908
Tânia Lopes <1516784+mctata@users.noreply.github.com>
2025-03-09 15:54:28 +0700

    Add database configuration

---
+++ backend/src/config/database.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialect: 'postgres',
+  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? {
+      require: true,
+      rejectUnauthorized: false
+    } : false
+  }
+});
+
+module.exports = { sequelize };
