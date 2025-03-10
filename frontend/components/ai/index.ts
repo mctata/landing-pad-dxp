7889d84c0b80e246f8f74511e59f0919ac4385ba
Tânia Lopes <1516784+mctata@users.noreply.github.com>
2025-03-10 13:22:02 +0700

    Add AI components index file

---
+++ frontend/components/ai/index.ts
@@ -0,0 +1,11 @@
+/**
+ * AI component exports
+ */
+
+export * from './types';
+export * from './AIAssistantButton';
+export * from './AIContentEditor';
+export * from './AIContentForm';
+export * from './AIPromptInput';
+export * from './AISuggestionPanel';
+export * from './AISuggestionPopover';
