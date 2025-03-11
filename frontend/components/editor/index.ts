c48cfd9177a51f8903b4d13deb81dbcbc48b364e
Tânia Lopes <1516784+mctata@users.noreply.github.com>
2025-03-11 11:11:05 +0700

    Update editor components to export AIElementPanel

---
+++ frontend/components/editor/index.ts
@@ -0,0 +1,4 @@
+export { EditorToolbar } from './EditorToolbar';
+export { EditorCanvas } from './EditorCanvas';
+export { default as PageElementsMenu } from './PageElementsMenu';
+export { default as AIElementPanel } from './AIElementPanel';
